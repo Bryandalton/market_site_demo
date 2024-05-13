5cef98a2e075ab9ed1ebd66726a58feb2761cb4d
Bryan Dalton <bryantdalton19@gmail.com>
2024-05-13 14:22:24 -0400

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,41 @@
-import { useState } from 'react'
-import './App.css'
-import styled from 'styled-components'
+import Basket_Logo from "../assets/basket_logo.png";
+import styled from "styled-components";
 
 const HeaderDiv = styled.div`
-    display: flex;
-    
+  display: grid;
+  grid-auto-flow: column;
+  gap: 3rem;
+  padding: 1rem;
+  border-bottom: .5rem solid var(--green);
 
-`
+  & img {
+    justify-self: start;
+    max-height: 5rem;
+    max-width: auto;
+  }
+
+  & h1 {
+    justify-self:center;
+    margin: 0;
+    font-size: 4rem;
+  }
+`;
+const LoginButton = styled.button`
+  justify-self: end;
+  max-height: 5rem;
+  max-width: auto;
+`;
 
 export default function Header() {
-    return(
-        <>
-        <HeaderDiv>
-        <img src='basket_logo.png'></img>
+  return (
+    <>
+      <HeaderDiv>
+        <img className="logo" src={Basket_Logo}></img>
         <h1>Fart Market</h1>
-        <h6>Login/Signup</h6>
-        </HeaderDiv>
-        </>
-    )
+        <LoginButton>
+          <h6>Login/Signup</h6>
+        </LoginButton>
+      </HeaderDiv>
+    </>
+  );
 }
